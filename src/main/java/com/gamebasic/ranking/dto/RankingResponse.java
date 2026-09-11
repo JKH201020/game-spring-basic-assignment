f4b24267f29ed830afc3785a57f198334f545c21
jkh <jack0517@naver.com>
2026-09-11 13:04:18 +0900

    fix: RankingResponse의 season 자료형을 Season에서 String으로 수정

---
+++ src/main/java/com/gamebasic/ranking/dto/RankingResponse.java
@@ -0,0 +1,16 @@
+package com.gamebasic.ranking.dto;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@RequiredArgsConstructor
+public class RankingResponse {
+
+    private final String season;
+    private final int totalRecords;
+    private final int excludedCount;
+    private final List<RankingEntriesResponse> entries;
+}
